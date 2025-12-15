6f1933bd98e664ca0badf2278ef0b3eb252847ed
Student <foobar@sr.htlweiz.at>
2025-12-15 11:18:16 +0100

    The whole Program is functional

---
+++ AverageCalculator/AverageCalculator.cs
@@ -0,0 +1,43 @@
+﻿namespace lib00;
+
+public class AverageCalculator
+{
+    public static double[] interndoubles = { };
+    private int interncount = 0;
+   private double sum = 0;
+
+    public void add(double value)
+    {
+        interndoubles = interndoubles.Append(value).ToArray();
+        interncount = interncount + 1;
+    }
+    public void add(double[] values)
+    {
+        for (int i = 0; i < values.Length; i++)
+        {
+            interndoubles = interndoubles.Append(values[i]).ToArray();
+            interncount = interncount + 1;
+        }
+
+    }
+    public int count()
+    {
+        return  interncount;
+    }
+    public double[] getElements()
+    {
+        return interndoubles;
+    }
+    public double getAverage()
+    {
+        for (int i = 0; i < interndoubles.Length; i++)
+        {
+            sum = sum + interndoubles[i];
+            
+        }
+        return  sum/interndoubles.Length;
+        
+    }
+    public string
+
+}
---
+++ Programs/Program00.cs
@@ -0,0 +1,26 @@
+﻿using lib00;
+
+namespace Program00
+{
+    public class Program00
+    {
+        public static int Main()
+        {
+            double[] startarray= {3.6,3.7};
+            var Calculator1 = new AverageCalculator();
+            Calculator1.add(2.3);
+            Calculator1.add(3.5);
+            Calculator1.add(startarray);
+            Calculator1.add(5.5);
+            int count = Calculator1.count();
+            System.Console.WriteLine(count);
+            double[] array = Calculator1.getElements();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            return 0;
+        }
+    }
+}
